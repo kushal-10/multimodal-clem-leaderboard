bd336342afc1f50fd6a68ee4030b3f35518f0d40
Sherzod Hakimov <sherzod-hakimov@users.noreply.huggingface.co>
2023-11-17 16:15:05 +0000

    Initial commit (#1)
    - Initial commit (d4c52034a11be912014a2ecc30b9c8a9d4ca6ba3)
    
    
    Co-authored-by: Koshti <Koshti10@users.noreply.huggingface.co>

---
+++ src/assets/text_content.py
@@ -0,0 +1,18 @@
+TITLE = """<h1 align="center" id="space-title"> 🏆 CLEM Leaderboard</h1>"""
+
+INTRODUCTION_TEXT = """
+🔝 The CLEM Leaderboard aims to track, rank and evaluate current cLLMs (chat-optimized Large Language Models, “clems”) as described in [Clembench: Using Game Play to Evaluate Chat-Optimized Language Models as Conversational Agents](https://arxiv.org/abs/2305.13455).
+"""
+
+SHORT_NAMES = {
+    "t0.0": "",
+    "claude-v1.3-": "cl",
+    "gpt-3.5-turbo-": "3.5",
+    "gpt-4-": "4",
+    "text-davinci-003-": "3",
+    "luminous-supreme-": "lm",
+    "koala-13b-": "ko",
+    "falcon-40b-": "flc",
+    "oasst-12b-": "ost",
+    "vicuna-13b-": "vcn"
+}
